5cc00eb05321fa647b9ed2ef1beec118b559c3b3
Sergio Peris <dev.sertxu@gmail.com>
2022-04-20 11:41:17 +0200

    Update Reboot Outlook.bat

--- Reboot Outlook.bat
+++ Reboot Outlook.bat
@@ -1,3 +1,7 @@
+:: Copyright (c) 2022 Grupo Peris
+:: @see https://github.com/grupoperis/reboot-outlook
+:: @author Sergio Peris <https://github.com/sertxudev>
+
 @echo off
 
 taskkill /F /IM outlook.exe /T
